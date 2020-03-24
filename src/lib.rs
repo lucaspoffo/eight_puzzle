160a6e3aebb5ca5856a1952cf5e26b35753e0548
Lucas Poffo <lucas.poffo@magrathealabs.com>
2020-03-24 19:41:55 -0300

    Added Game Tree to add tree visualization next, add solver with step function so we can stop executing when we want.

--- src/lib.rs
+++ src/lib.rs
@@ -55,7 +55,7 @@ impl State {
     
     fn move_empty_space(&self, to: Vector2, empty_space: Vector2) -> Board {
         let mut board = self.board.clone();
-        board.0[empty_space.0][empty_space.1] = board.0[to.0][to.1];;
+        board.0[empty_space.0][empty_space.1] = board.0[to.0][to.1];
         board.0[to.0][to.1] = 0;
         board
     }
@@ -89,34 +89,127 @@ impl State {
     }
 }
 
-pub fn resolve(initial_board: Board, goal: &Board, heuristic: &dyn Fn(&Board, &Board) -> usize) -> Vec<State> {
-    let inital_state = State::new(initial_board, 0, 0, None);
+#[derive(Debug)]
+struct ArenaTree<T> where T: PartialEq + Clone {
+    arena: Vec<Node<T>>
+}
 
-    let mut open_set: Vec<State> = vec![];
-    let mut came_from: HashMap<State, State> = HashMap::new();
-    open_set.push(inital_state);
+#[derive(Debug, Clone)]
+struct Node<T> where T: PartialEq + Clone {
+    index: usize,
+    val: T,
+    parent: Option<usize>,
+    children: Vec<usize>
+}
 
-    while !open_set.is_empty() {
-        if let Some(&current) = open_set.iter().min_by(|x, y| x.cost.cmp(&y.cost)) {
-            if &current.board == goal {
-                let mut path = vec![];
-                path.push(current);
-                let mut from = current;
-                while let Some(&x) = came_from.get(&from) {
-                    path.push(x);
-                    from = x;                    
-                }
-                return path;
+impl<T> Node<T> where T: PartialEq + Clone {
+    fn new(index: usize, val: T, parent: Option<usize>) -> Self {
+        Self { index, val, parent, children: vec![] }
+    }
+}
+
+impl<T> ArenaTree<T> where T: PartialEq + Clone {
+    fn new() -> Self {
+        ArenaTree { arena: vec![] }
+    }
+
+    fn get_index(&self, val: &T) -> Option<usize> {
+        for node in &self.arena {
+            if &node.val == val {
+                return Some(node.index);
             }
-            open_set.retain(|&b| b != current);
-            let neighbors = current.next_states(goal, heuristic);
-            for n in neighbors {
-                came_from.insert(n, current);
-                open_set.push(n);
+        }
+        None
+    }
+
+    fn get_node(&self, index: usize) -> Option<&Node<T>> {
+        if index < self.arena.len() {
+            return Some(&self.arena[index]);
+        }
+        None
+    }
+    
+    fn insert(&mut self, val: T, parent_index: Option<usize>) {
+        let index = self.arena.len();
+        self.arena.push(Node::new(index, val, parent_index));
+        if let Some(parent_index) = parent_index {
+            self.arena[parent_index].children.push(index);
+        }
+    }
+
+    fn root_path(&self, index: usize) -> Vec<T> {
+        let mut path: Vec<T> = vec![];
+        let mut current = self.get_node(index);
+        while let Some(c) = current {
+            path.push(c.val.clone());
+            if let Some(parent) = c.parent {
+                current = self.get_node(parent);
+            } else {
+                current = None;
+            }
+        }
+        path
+    }
+}
+
+pub struct Solver<'a> {
+    initial_board: Board,
+    goal: Board,
+    heuristic: &'a dyn Fn(&Board, &Board) -> usize,
+    game_tree: ArenaTree<Board>,
+    frontier: Vec<State>,
+    result_path: Vec<Board>,
+    solved: bool,
+}
+
+impl Solver<'_> {
+    pub fn new(initial_board: Board, goal: Board, heuristic: &dyn Fn(&Board, &Board) -> usize) -> Solver {
+        let mut game_tree: ArenaTree<Board> = ArenaTree::new();
+        game_tree.insert(initial_board, None);
+
+        let mut frontier = vec![];
+        let initial_state = State::new(initial_board, 0, 0, None);
+        frontier.push(initial_state);
+
+        Solver { initial_board, goal, heuristic, game_tree, frontier, solved: false, result_path: vec![] }
+    }
+
+    pub fn step(&mut self) {
+        if !self.solved && !self.frontier.is_empty() {
+            if let Some(&current) = self.frontier.iter().min_by(|x, y| x.cost.cmp(&y.cost)) {
+                if current.board == self.goal {
+                    self.solved = true;
+                    if let Some(current_index) = self.game_tree.get_index(&current.board) {
+                        self.result_path = self.game_tree.root_path(current_index);
+                    }
+                    return;
+                }
+                self.frontier.retain(|&b| b != current);
+                let parent_index = self.game_tree.get_index(&current.board);
+                let neighbors = current.next_states(&self.goal, self.heuristic);
+                for n in neighbors {
+                    self.game_tree.insert(n.board, parent_index);
+                    self.frontier.push(n);
+                }
             }
         }
     }
-    vec![]
+
+    pub fn is_solved(&self) -> bool {
+        self.solved
+    }
+
+    pub fn result_path(&self) -> &Vec<Board> {
+        &self.result_path
+    }
+}
+
+pub fn resolve(initial_board: Board, goal: Board, heuristic: &dyn Fn(&Board, &Board) -> usize) -> Vec<Board> {
+    let mut solver = Solver::new(initial_board, goal, &heuristic);
+    while !solver.is_solved() {
+        solver.step();
+    }
+    solver.result_path().clone()
 }
 
 pub fn hamming(goal: &Board, board: &Board) -> usize {
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use eight_puzzle::{resolve, hamming, Board};
+use eight_puzzle::{hamming, Board, Solver};
 
 
 fn main() {
     let initial_board: Board = Board::new([[1, 6, 2], [5, 7, 3], [0, 4, 8]]);
     let goal: Board = Board::new([[1, 2, 3], [4, 5, 6] ,[7, 8, 0]]);
 
-    let moves = resolve(initial_board, &goal, &hamming);
-    for m in moves {
-        println!("{}", m.board);
+    let mut solver = Solver::new(initial_board, goal, &hamming);
+    while !solver.is_solved() {
+        solver.step();
+    }
+
+    for b in solver.result_path() {
+        println!("{}", b);
         println!("-----");
     }
 }
